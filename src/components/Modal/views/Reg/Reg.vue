f94b0c9c538037b6994b948dbc47a40742156170
mrJO1N <zhenduk.zhenduk123456780@gmail.com>
2025-05-25 21:58:42 +0300

    reg-modal-test

--- src/components/Modal/views/Reg/Reg.vue
+++ src/components/Modal/views/Reg/Reg.vue
@@ -1,7 +1,38 @@
-<template>html</template>
+<template>
+  <div class="modal-wrapper">
+    <div class="modal">
+      <span class="close">x</span>
+      <h2>Регистрация</h2>
+
+      <div class="form-group">
+        <label for="email">email</label>
+        <input type="email" id="email" placeholder="email" v-model="email">
+      </div>
+
+      <div class="form-group">
+        <label for="password">пароль</label>
+        <input type="password" id="password" placeholder="password" v-model="password">
+      </div>
+
+      <div class="form-group remember-me">
+        <label>
+          <input type="checkbox" v-model="rememberMe"> Запомнить меня
+        </label>
+      </div>
+
+      <button class="submit-btn" @click="register">Зарегистрироваться</button>
+      <div class="switch-link">
+        Уже есть аккаунт?
+        <a href="#">Войти</a>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts" setup>
 interface IProps {}
+import "./Reg.scss"
+
 
 defineProps<IProps>();
 </script>
