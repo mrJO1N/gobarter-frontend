f4a4c72eba67ba9d82f7ccf244925abe7013ce7e
Ganynany <125253169+Ganynany@users.noreply.github.com>
2025-05-25 23:56:16 +0300

    Add files via upload

---
+++ Reg.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <span class="close">x</span>
+      <h2>Регистрация</h2>
+
+      <label for="email">email</label>
+      <Input visibility-type ="email" placeholder="email" />
+
+      <label for="password">пароль</label>
+      <Input visibility-type ="password" type="password" placeholder="password" />
+
+      <label class="checkbox-wrapper">
+        <input type="checkbox" checked />
+        Запомнить меня
+      </label>
+
+      <button class="submit-btn">Зарегистрироваться</button>
+      <div class="switch-link">Авторизация профиля</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import "./Reg.scss"
+import "@ui/Input"
+interface IProps {}
+defineProps<IProps>();
+</script>
+
+<style lang="sass" scoped></style>
